05889023f1ef953ef4fe9f93f50c5a13bfd4a192
selsmie <simonelsmie@hotmail.com>
2021-04-18 15:54:30 +0100

    header styled

--- src/components/HeaderAndCountryFilter.js
+++ src/components/HeaderAndCountryFilter.js
@@ -1,11 +1,31 @@
 import CountryList from './CountryList'
+import './HeaderAndCountryFilter.css'
 
 const HeaderAndCountryFilter = ({countries, onCountrySelect}) => {
     return (
-        <>
-            <p>headercountryfilter</p>    
-            <CountryList countries={countries} onCountrySelect={onCountrySelect}/>        
-        </>
+        <header>
+            <div className="title-links">
+                <div className="budget">
+                    <a href="/">Budgeting</a>
+                </div>
+                <h2 id="bud" className="title">Bud<span id="getaway" className="title">getAway</span></h2>
+                <div className="navbar">
+                    <a href="/">Home</a>
+                    <a href="/">Wishlist</a>
+                    <a href="/">About</a>
+                </div>
+            </div>
+
+            <form className="search-bar">
+                <select name="lanugages" id="languages">
+                    <option value="disabled">Select Language</option>
+                </select>
+                <select name="regions" id="regions">
+                    <option value="disabled">Select Region</option>
+                </select>
+                <CountryList countries={countries} onCountrySelect={onCountrySelect}/> 
+            </form>     
+        </header>
     )
 }
 
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,12 +1,9 @@
-// import CountryList from '../components/CountryList'
 import SelectedCountryDetail from '../components/SelectedCountryDetail'
 
 const CountryContainer = ({selectedCountry, onSearchSubmit}) => {
     
     return (
         <div>
-            <p>countrycontainer</p>
-            {/* <CountryList countries={countries} onCountrySelect={onCountrySelect}/> */}
             <SelectedCountryDetail selectedCountry={selectedCountry} onSearchSubmit={onSearchSubmit}/>
         </div>
     )
