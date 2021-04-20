384fffd671e9252494c65fac07798c33d605ecae
Jenny Bulford <jennycbx@hotmail.com>
2021-04-20 12:54:08 +0100

    Added sidepanel

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -9,26 +9,3 @@ const Budget = () => (
 export default Budget
 
 
-// import SideBar from "../components/SideBar";
-// import Content from "../components/Content";
-
-// class SideDrawer {handleViewSidebar} {
-//     state = {
-//         sidebarOpen: false
-//     };
-//     handleViewSidebar = () =>
-//         this.setState({ sidebarOpen: !this.state.sidebarOpen })
-
-  
-//     return (
-//       <div>
-//         <SideBar
-//           isOpen={this.state.sidebarOpen}
-//           toggleSidebar={this.handleViewSidebar}
-//         />
-//         <Content isOpen={this.state.sidebarOpen} />
-//       </div>
-//     );
-// }
-
-// export default SideDrawer;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,14 +1,28 @@
-// class SideBar = ({toggleSidebar}) => {
-//   render() {
-//     var sidebarClass = this.props.isOpen ? "sidebar open" : "sidebar";
-//     return (
-//       <div className={sidebarClass}>
-//         <div>I have slided</div>
-
-//         <button onClick={this.props.toggleSidebar} className="sidebar-toggle">Sidebar</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default SideBar;
+import React, { useState } from 'react';
+import SlidingPanel from 'react-sliding-side-panel';
+import './SideBar.css'
+import Budget from './Budget.js'
+ 
+const SideBar= () => {
+  const [openPanel, setOpenPanel] = useState(false);
+  return (
+    <section>
+      <div>
+        <button onClick={() => setOpenPanel(true)}>Budget</button>
+      </div>
+      <SlidingPanel
+        id='sidebar'
+        type={'left'}
+        isOpen={openPanel}
+        size={20}
+      >
+        <div id='panel-content'>
+            <Budget/>
+            <button onClick={() => setOpenPanel(false)}>close</button>
+        </div>
+      </SlidingPanel>
+    </section>
+  );
+};
+ 
+export default SideBar;
