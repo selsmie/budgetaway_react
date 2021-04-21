10bfed37d859072ac529e88e58aa6e1dcdd74ed6
rabisse <rabisse@gmail.com>
2021-04-21 09:55:21 +0100

    languages sort alphabetically

--- src/components/HeaderAndCountryFilter.js
+++ src/components/HeaderAndCountryFilter.js
@@ -6,27 +6,17 @@ import logo from "../logo.png"
 const HeaderAndCountryFilter = ({countries, onCountrySelect, luckyDip, languages, onLanguageChange}) => {
 
     const budgetClick = () => {
-
     }
 
     const handleLuckyDip = (evt) => {
         luckyDip()
     }
 
-    const displayEnglish = (languages) ? languages.map((language, id) => {
-        if (language.name === "English"){
+    const displayLanguages = (languages) ?
+        languages.sort((a, b) => a.name.localeCompare(b.name))
+        .map((language, id) => {
             return <option value={language.name} key={id}>{language.name}</option>
-        }
-        return null
-    }) : null
-
-    const displayLanguages = (languages) ? 
-    languages.map((language, id) => {
-        if (language.name === "English"){
-            return null
-        }
-        return <option value={language.name} key={id}>{language.name}</option>
-    }) : null
+        }) : null
 
     const handleLanguageSelect = (evt) => {
         onLanguageChange(evt.target.value)
@@ -44,7 +34,6 @@ const HeaderAndCountryFilter = ({countries, onCountrySelect, luckyDip, languages
             <div className="search-bar">
                     <select name="lanugages" id="languages" onChange={handleLanguageSelect}>
                         <option value="disabled">Select Language</option>
-                        {displayEnglish}
                         {displayLanguages}
                     </select>
                     <select name="regions" id="regions">
