f4cee2a150b4809c1643377d58eea6fc342241b9
Jan Metelski <janekmetelski@hotmail.com>
2021-04-22 15:08:14 +0100

    Worked on flights display

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,10 +1,28 @@
 import "./Wishlist.css"
 
-const Wishlist = () => (
-    <div className="wishlist">
-        <h4>Wishlist</h4>
-        <p>Lots of countries YAY!</p>
-    </div>
-)
+const Wishlist = (flight, wishlist) => {
+
+    const wishlistDisplay = (wishlist) ? 
+        wishlist.map((wishlist, index) => {
+            return (
+
+            <div className = "flight">
+                <h2 id="flight-name">{flight.name}</h2>
+                <div id="flight-details">
+                    <p>Departure Airport: {flight.depAirport}</p>
+                    <p>Arrival Airport: {flight.arrAirport}</p>
+                    <p>Duration: {flight.duration}</p>
+                    <p>Price: {flight.price}</p>
+                </div>
+            </div>
+            )
+    }) : null
+
+    return (
+        <div className="wishlist">
+            {wishlistDisplay}
+        </div>
+    )
+}
 
 export default  Wishlist;
